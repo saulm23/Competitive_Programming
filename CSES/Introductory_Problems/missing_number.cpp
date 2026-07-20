3d2115391a12d59db02657d2bf090b34b749cb3d
saulm23 <marcasaul00@gmail.com>
2026-07-20 12:47:42 -0400

    solved missing number

--- CSES/Introductory_Problems/missing_number.cpp
+++ CSES/Introductory_Problems/missing_number.cpp
@@ -1,9 +1,17 @@
-#include <bits/stc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-	
-	
+	long long n, sum, j, r;
+	cin >> n;
+	sum = (n * (n + 1)) / 2;
+	r = 0;
+	for(int i = 0; i < n - 1; ++i)
+	{
+		cin >> j;
+		r = r + j;
+	}
+	cout << sum - r << endl;
 }
