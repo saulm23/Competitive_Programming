b2248b42a8de68f40c60d10c3f0af3693cfce8c3
saulm23 <marcasaul00@gmail.com>
2026-08-03 19:20:45 -0400

    Solved number spiral

--- CSES/Introductory_Problems/number_spiral.cpp
+++ CSES/Introductory_Problems/number_spiral.cpp
@@ -4,30 +4,40 @@ long diagonal(long n);
 
 int main()
 {
-	long t, ans, x, y;
+	long t, ans, x, y, diagonal;
 	scanf("%ld", &t);		
 	for(long i = 1; i <= t; ++i)
 	{
 		scanf("%ld %ld", &y, &x);
-		if(x > y) 
+		if(x > y)
 		{
-			ans = diagonal(x) + (-2 * (x % 2) + 1) ;
+			diagonal = x * x - x + 1;	
+			ans = diagonal + (2 * (x % 2) - 1) * (x - y);
 		}
 		else if(y > x)
 		{
-			ans = diagonal(y) + (-2 * (y % 2) + 1) * x - 1;
+			diagonal = y * y - y + 1;	
+			ans = diagonal + (-2 * (y % 2) + 1) * (y - x);
 		}
-		else ans = diagonal(x);
+		else ans = x * x - x + 1;
+		
 		printf("%ld\n", ans);
 	}
 
 }
-long diagonal(long n)
-{
-	long ans = 1;
-	for(long i = 0; i < n; ++i)
-	{
-		ans = ans + 2 * i;
-	}
-	return ans;
-}
+/*
+0 -> -1
+1 -> +1
+0 -> 1
+1 -> -1
+
+0 * x + y = +1 => y = 1
+1 * x + y = -1 => x = -2
++
+- (n - y)
+if x is even 
+then go -- 
+if x is odd
+then go ++
+
+*/
