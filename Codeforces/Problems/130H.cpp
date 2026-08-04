1d004c58eadc3c120b1be48c4249a6fdef131459
saulm23 <marcasaul00@gmail.com>
2026-08-03 20:03:06 -0400

    Started to solve 130H:Balanced Brackets

---
+++ Codeforces/Problems/130H.cpp
@@ -0,0 +1,22 @@
+/*
+((((((())))))) -> yes
+(((()))()) -> yes
+()()()()()() -> yes
+((())) -> yes
+
+string of chars
+(  (  (  )  (  )
+o1 o2 o3 c3 o4 c4
+(  )  (  (  )  )
+o1 c1 o2 o3 c3 c2
+
+for char in string
+	char can be ( or )
+	if char1 is ) -> print no
+	if sring length is odd -> print no	
+
+	
+else if char1 is (
+
+then char2 is ( or )
+*/
